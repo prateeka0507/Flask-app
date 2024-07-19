ee7411dfe554bab5ac9409d23ff73d598edff3b9
prateeka0507 <123346817+prateeka0507@users.noreply.github.com>
2024-07-19 14:11:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,18 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/process_button', methods=['POST'])
+def process_button():
+    button = request.form['button']
+    if button == 'button1':
+        message = "Button 1 was clicked!"
+    elif button == 'button2':
+        message = "Button 2 was clicked!"
+    elif button == 'button3':
+        message = "Button 3 was clicked!"
+    else:
+        message = "Unknown button clicked!"
+    return render_template('output.html', message=message)
 
 if __name__ == '__main__':
     app.run(debug=True)
